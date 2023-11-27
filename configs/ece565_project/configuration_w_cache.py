87560e219ef700ad355e1c1835caa375675fcaa1
Marco Apolinario <mapolina@purdue.edu>
2023-11-27 18:08:10 -0500

    mem: L3 configuration
    Change-Id: I2bf32b81b2e906bf3d19931e22333714573c7165

---
+++ configs/ece565_project/configuration_w_cache.py
@@ -0,0 +1,74 @@
+import m5
+from m5.objects import *
+from cache_config import *
+
+system = System()
+
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = '1GHz'
+system.clk_domain.voltage_domain = VoltageDomain()
+
+system.mem_mode = 'timing'
+system.mem_ranges = [AddrRange('512MB')]
+
+system.cpu = X86TimingSimpleCPU()
+
+
+# system.cpu.icache_port = system.membus.cpu_side_ports
+# system.cpu.dcache_port = system.membus.cpu_side_ports
+
+system.cpu.icache = L1ICache()
+system.cpu.dcache = L1DCache()
+
+system.cpu.icache.connectCPU(system.cpu)
+system.cpu.dcache.connectCPU(system.cpu)
+
+system.l2bus = L2XBar()
+system.cpu.icache.connectBus(system.l2bus)
+system.cpu.dcache.connectBus(system.l2bus)
+
+system.l2cache = L2Cache()
+system.l2cache.connectCPUSideBus(system.l2bus)
+system.l3bus = L2XBar()
+system.l2cache.connectMemSideBus(system.l3bus)
+
+system.l3cache = L3Cache()
+system.l3cache.connectCPUSideBus(system.l3bus)
+system.membus = SystemXBar()
+system.l3cache.connectMemSideBus(system.membus)
+
+
+
+system.cpu.createInterruptController()
+system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+
+system.system_port = system.membus.cpu_side_ports
+
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+binary = 'tests/test-progs/hello/bin/x86/linux/hello'
+
+# for gem5 V21 and beyond
+system.workload = SEWorkload.init_compatible(binary)
+
+process = Process()
+process.cmd = [binary]
+system.cpu.workload = process
+system.cpu.createThreads()
+
+# max number of instruction for simulation
+system.cpu.max_insts_any_thread = 1e7
+
+root = Root(full_system = False, system = system)
+m5.instantiate()
+
+print("Beginning simulation!")
+exit_event = m5.simulate()
+
+print('Exiting @ tick {} because {}'.format(
+    m5.curTick(), exit_event.getCause()))
